c606910230af201d5cf30dda6d22dc8e5c124041
42arch <rend42@163.com>
2024-09-13 17:29:49 +0800

    Initial commit

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,27 @@
-"use client";
-
-import { ReactNode } from "react";
-
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export function Button({ children, ...other }: ButtonProps): JSX.Element {
+  const a = 11
+
   return (
     <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      type='button'
+      {...other}
+      style={{
+        outline: 'none',
+        backgroundColor: '#ffeeee',
+        border: '1px sold #444444',
+        borderRadius: '4px',
+        padding: '4px 8px',
+        cursor: 'pointer'
+      }}
     >
       {children}
     </button>
-  );
-};
+  )
+}
+
+Button.displayName = 'Button'
